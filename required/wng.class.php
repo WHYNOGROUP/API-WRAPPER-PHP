35b14d580e6035fe9804274303f29573d420d3f0
Kévin Paly <39833272+PALYBOK@users.noreply.github.com>
2018-06-09 19:35:16 +0200

    Add files via upload

--- required/wng.class.php
+++ required/wng.class.php
@@ -1,5 +1,4 @@
 <?php
-
 # GNU LESSER GENERAL PUBLIC LICENSE
 # Version 3, 29 June 2007
 #
@@ -8,7 +7,6 @@
 # of this license document, but changing it is not allowed.
 # WHYNOGROUP - WNG - WNL - WNA - WNH - WNR - WNC
 # 2018-06-02 - https://api.whyno.group
-
 /**
  * Wrapper to manage login and exchanges with Wng API.
  * Api wng queries, are done in curl php.
@@ -26,7 +24,6 @@ class wng
         'wng-ca'        => 'https://api-ca.whyno.group',
         'wng-us'        => 'https://api-us.whyno.group',
     ];
-
     /**
      * Contain endpoint selected, in a futur version,
      * we will implement a cdn that will automatically
@@ -35,42 +32,48 @@ class wng
      * @var string
      */
     private $endpoint = 'https://alpha.api-eu.whyno.group';
-
     /**
      * Contain consumer key of the current application
      *
      * @var string
      */
     private $consumer_key = null;
-
     /**
      * Contain curl client connection
      *
      * @var Client
      */
     private $curl_client = null;
-
     /**
      * Contain the data table ready to be sent to the API
      *
      * @var array
      */
     private $input = null;
-
     /**
      * Contains the path for the requested API
      *
      * @var URI
      */
+    private $file = null;
+    /**
+     * Contains the path of the temp folder
+     *
+     * @var path
+     */
+    private $temp_folder = '/temp/';
+    /**
+     * Contains the file of user
+     *
+     * @var file
+     */
     private $path = null;
-
     /**
      * Contains the method for the requested API
      *
-     * @var URI
+     * @var string
      */
     private $method = null;
-
     /**
      * Construct a new instance
      *
@@ -79,20 +82,14 @@ class wng
      * @throws \Exception if one parameter is missing or with bad value
      */
     public function __construct($consumer_key = null) {
-
       try {
-
         if(!isset($consumer_key) || empty($consumer_key)){
           throw new \Exception("InvalidParameterException: missing consumer_key");
         }
-
         $this->consumer_key         = $consumer_key;
         return true;
-
       } catch (\Exception $e) { return $e->getMessage(); }
-
     }
-
     /**
      * Wrap call to Wng APIs for GET requests
      *
@@ -102,54 +99,46 @@ class wng
      * @return array
      */
       public function get($path, $input = null){
-
         $this->path                 = $path;
         $this->input                = $input;
         $this->method               = "GET";
-
                $this->callApi();
         return $this->output;
-
       }
-
     /**
      * Wrap call to Wng APIs for PUT requests
      *
      * @param string $path path ask inside api
      * @param array $input content to send inside body of request
+     * @param array $file content type file
      *
      * @return array
      */
-      public function put($path, $input = null){
-
+      public function put($path, $input = null, $file = null){
         $this->path                 = $path;
         $this->input                = $input;
+        $this->file                 = $file;
         $this->method               = "PUT";
-
                $this->callApi();
         return $this->output;
-
       }
-
     /**
      * Wrap call to Wng APIs for POST requests
      *
      * @param string $path path ask inside api
      * @param array $input content to send inside body of request
+     * @param array $file content type file
      *
      * @return array
      */
-      public function post($path, $input = null){
-
+      public function post($path, $input = null, $file = null){
         $this->path                 = $path;
         $this->input                = $input;
+        $this->file                 = $file;
         $this->method               = "POST";
-
                $this->callApi();
         return $this->output;
-
       }
-
     /**
      * Wrap call to Wng APIs for DELETE requests
      *
@@ -159,79 +148,72 @@ class wng
      * @return array
      */
       public function delete($path, $input = null){
-
         $this->path                 = $path;
         $this->input                = $input;
         $this->method               = "DELETE";
-
                $this->callApi();
         return $this->output;
-
       }
-
       /**
        * Convert input data in php array format to json array format
        *
        * @return array json format
        */
         public function prepareData(){
-
           return json_encode($this->input);
-
         }
-
       /**
-       * Prepare file to send api - BETA DON'T USE IT!
+       * Prepare file to send api
        *
        * @return object
        */
         public function prepareFile(){
-         
-         //We will add here a storage system in the temporary folder
-            
-         $file = 'whynogroup.jpg';
-         return new CURLFile(
-             getcwd().'/temp/'.$file,
-             mime_content_type(getcwd().'/temp/'.$file),
-             $file
-         );
+
+          if(empty($this->file))
+          {
+            return null;
+          }
+          else
+          {
+            return new CURLFile(
+                getcwd().$this->temp_folder.$this->file,
+                mime_content_type(getcwd().$this->temp_folder.$this->file),
+                $this->file
+            );
+          }
 
         }
+      /**
+       * Clean file after send it
+       */
+        public function cleanFile(){
 
+          unlink(getcwd().$this->temp_folder.$this->file);
+          unset($this->file);
+
+        }
       /**
        * Prepare header of curl client
        *
        * @return array
        */
         public function prepareHeaders(){
-
             return array(
-
               "cache-control: no-cache",
-
               "x-wng-consumer: {$this->consumer_key}",
-
               "x-wng-endpoint: {$this->endpoint}",
-
               "x-wng-method: {$this->method}"
-
             );
-
         }
-
       /**
        * Send request to Api
        *
        * @return array
        */
         public function callApi(){
-
           $this->curl_client          = curl_init();
-
           curl_setopt_array(
-
             $this->curl_client,
-
             array(
               CURLOPT_URL             => $this->endpoint . $this->path,
               CURLOPT_RETURNTRANSFER  => true,
@@ -243,24 +225,18 @@ class wng
               CURLOPT_POSTFIELDS      => array('userfile' =>  $this->prepareFile(), 'userdata' => $this->prepareData()),
               CURLOPT_HTTPHEADER      => $this->prepareHeaders(),
             )
-
           );
-
           $o                          = curl_exec($this->curl_client);
           $e                          = curl_error($this->curl_client);
-
           curl_close($this->curl_client);
-
           if ($e) {
-
             $this->output               = "WNG API-EU CLIENT: " . $e;
-
           } else {
-
+            if(!empty($this->file))
+            {
+              $this->cleanFile();
+            }
             $this->output               = json_decode($o, true);
-
           }
-
         }
-
 }
